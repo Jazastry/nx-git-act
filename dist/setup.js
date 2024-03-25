ff77e31951e6ed0c8f76be26b39bda14983746d6
Jazastry <jazastry@gmail.com>
2024-03-25 21:39:04 +0200

    bundles

---
+++ dist/setup.js
@@ -0,0 +1,16 @@
+import { execSync } from 'node:child_process';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'url';
+const pathA = resolve(dirname(process.execPath), 'npm');
+console.log('pathA:', pathA);
+// j&& ${pathA} install -g yarn
+execSync(`sudo ln -sf "${process.execPath}" /usr/bin/node`, {
+    stdio: 'inherit',
+    encoding: 'utf-8'
+});
+const pathB = fileURLToPath(import.meta.resolve('..'));
+console.log('pathB:', pathB);
+// execSync(`yarn --cwd ${pathB}`, {
+//   stdio: 'inherit',
+//   encoding: 'utf-8'
+// })
